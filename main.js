1e8ca7757b3fef9d5cc7abb56f9f364d67839735
furylibre <142692160+fury-libre@users.noreply.github.com>
2025-12-08 20:31:11 -0600

    Update main.js

--- main.js
+++ main.js
@@ -102,7 +102,7 @@ function render() {
 
 
 
-/*
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -169,4 +169,5 @@ function animate() {
   renderer.render( scene, camera );
 
 }
+
 */
